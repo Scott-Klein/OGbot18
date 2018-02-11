b904290ecb05c2e6d0566942dd69e62fa8b69a05
Scott Klein <scotty.klei@gmail.com>
2018-02-11 15:19:22 +1100

    Added some http headers and began the web client

--- OGbot/Program.cs
+++ OGbot/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,6 +11,20 @@ namespace OGbot
     {
         static void Main(string[] args)
         {
+            string myParameters = "login=scotty.klei@gmail.com&pass=Avignale3&s149-en.ogame.gameforge.com";
+            using (WebClient client = new WebClient())
+            {
+                client.Headers[HttpRequestHeader.ContentType] = "application / x - www - form - urlencoded";
+                client.Headers[HttpRequestHeader.UserAgent] = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/64.0.3282.140 Safari/537.36";
+                client.Headers[HttpRequestHeader.Accept] = "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8";
+                client.Headers[HttpRequestHeader.Referer] = "https://en.ogame.gameforge.com/";
+                client.Headers[HttpRequestHeader.AcceptEncoding] = "gzip, deflate, br";
+                client.Headers[HttpRequestHeader.AcceptLanguage] = "en-US,en;q=0.9";
+
+
+
+                string htmlReturned = 
+            }
         }
     }
 }
